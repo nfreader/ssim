8786fa2f9889991fb5504af8c5bb09da1b68f1e6
Nick Farley <nick@nfreader.net>
2015-11-06 12:08:45 -0500

    Restored change log functionality
    Woo!

--- view/about.php
+++ view/about.php
@@ -1,11 +1,9 @@
 <?php
-
 include '../inc/config.php';
-$pilot = new pilot();
-$spob = new spob($pilot->pilot->spob);
+require_once('rightbar.php');
 ?>
 
-<div class="fiftyfifty">
+<div class="leftbar">
 <h1>About Spacesim</h1>  
   <ul class="options">
     <li><a href='home' class='page'>Back</a></li>
@@ -14,25 +12,23 @@ $spob = new spob($pilot->pilot->spob);
 </div>
 <?php
 
-$changes = array(
-  array(
-    'class'=>'olive',
-    'content'=>'Added a changelog!'
-  ));
+$changes[] = array(
+  'class'=>'olive',
+  'content'=>'Added a changelog!'
+);
+
+$changes[] = array(
+  'class'=>'olive',
+  'content'=>'Restored messaging functionality'
+);
 
 ?>
 
-<div class="fiftyfifty"><h1>Changelog</h1>
+<div class="center"><h1>Changelog</h1>
   <ul class="options changelog">
     <?php foreach ($changes as $change) {
       echo "<li class='color ".$change['class']."'> ".$change['content']."</li>";
     } ?>
   </ul>
 </div>
-
-<?php include 'rightbar.php'; ?>
-<script>
-  loadContent('ping', '.footer', '.footerbar');
-</script>
-
 
