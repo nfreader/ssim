43fe00e580590707a75b62d66523a55f5e856994
Nick Farley <nick@nfreader.net>
2015-11-26 12:20:27 -0500

    Government Work
    Starting the foundations for Combat \o/

--- inc/classes/govt.php
+++ inc/classes/govt.php
@@ -1,33 +1,93 @@
-<?php 
+<?php
 
 class govt {
 
-  public function listGovt($id=null) {
-    $db = new database();
-    if ($id === null) {
-      $db->query("SELECT * FROM ssim_govt");
-      $db->execute();
-      return $db->resultSet();
-    } else {
-      $db->query("SELECT * FROM ssim_govt WHERE id = :id");
-      $db->bind(':id',$id);
-      $db->execute();
-      return $db->single();
+  public $id;
+  public $name;
+  public $isoname;
+  public $type;
+  public $color1;
+  public $color2;
+
+  public $relations;
+
+  public function __construct($id=null) {
+    if($id){
+      $govt = $this->getGovt($id);
+      $this->id = $govt->id;
+      $this->name = $govt->name;
+      $this->isoname = $govt->isoname;
+      $this->color1 = $govt->color1;
+      $this->color2 = $govt->color2;
+
+      if('I' === $govt->type) {
+        $this->type = 'Independent';
+      } elseif('P' === $govt->type) {
+        $this->type = 'Pirate';
+      } else {
+        $this->type = 'Regular';
+      }
+      $this->relations = $this->getRelations($this->id);
     }
+  }
+
+  public function getGovt($id) {
+    $db = new database();
+    $db->query("SELECT * FROM tbl_govt WHERE id = ?");
+    $db->bind(1,$id);
+    try {
+      $db->execute();
+    } catch (Exception $e) {
+      return returnError("Database error: ".$e->getMessage());
+    }
+    return $db->single();
+  }
+
+  public function getGovts() {
+    $db = new database();
+    $db->query("SELECT * FROM tbl_govt");
+    $db->execute();
+    return $db->resultSet();
   }
 
   public function getIndieGovt() {
     $db = new database();
-    $db->query("SELECT id FROM ssim_govt WHERE type = 'I'");
+    $db->query("SELECT id FROM tbl_govt WHERE type = 'I'");
     $db->execute();
     return $db->single()->id;
   }
 
   public function getPirateGovt() {
     $db = new database();
-    $db->query("SELECT id FROM ssim_govt WHERE type = 'P'");
+    $db->query("SELECT id FROM tbl_govt WHERE type = 'P'");
     $db->execute();
     return $db->single()->id;
+  }
+
+  public function getRelations($id) {
+    $db = new database();
+    $db->query("SELECT ssim_govtrelations.*,
+      target.name AS tgtname,
+      target.isoname AS tgtisoname,
+      target.color1 AS tgtcolor1,
+      target.color2 AS tgtcolor2,
+      subject.name AS subjname,
+      subject.isoname AS subjisoname,
+      subject.color1 AS subjcolor1,
+      subject.color2 AS subjcolor2
+      FROM ssim_govtrelations
+      LEFT JOIN ssim_govt AS target ON ssim_govtrelations.target = target.id
+      LEFT JOIN ssim_govt AS `subject` ON ssim_govtrelations.subject = subject.id
+      WHERE tbl_govtrelations.subject = ?
+      OR tbl_govtrelations.target = ?");
+    $db->bind(1,$id);
+    $db->bind(2,$id);
+    try {
+      $db->execute();
+    } catch (Exception $e) {
+      return returnError("Database error: ".$e->getMessage());
+    }
+    return $db->resultset();
   }
 
 }
--- view/admin/government.php
+++ view/admin/government.php
@@ -1,20 +1,39 @@
-<?php 
+<?php
 include 'adminHeader.php';
 
-$govt = new govt();
-$governments = $govt->listGovt();
 
+if(isset($_GET['govtid'])) :
+  $govt = new govt($_GET['govtid']);
+  ?>
+  <div class="center wide">
+    <h1><?php echo $govt->name;?></h1>
+    <span class="fingerprint"><?php echo $govt->type;?> Government</span>
+    <h2>Relations</h2>
+    <?php foreach ($govt->relations as $relation) : ?>
+      <?php if($govt->id == $relation->subject) :?>
+        <?php echo $relation->relation;?> with <?php echo $relation->tgtname;?><br>
+      <?php else: ?>
+      <?php echo $relation->relation;?> with <?php echo $relation->subjname;?><br>
+    <?php endif;?>
+    <?php endforeach;?>
+</div>
+  <?php
+else:
+  $govt = new govt();
+  $governments = $govt->getGovts();
 ?>
 
-<div class='rightbar'>
-
-</div>
-
-<div class="center">
+<div class="center wide">
   <h1>Governments</h1>
   <ul class="options">
-  <?php foreach ($governments as $govt) {
-    echo "<li><a href='' style='background:".$govt->color1."; color:".$govt->color2."'>".$govt->name."</a></li>";
-  } ?>
+    <?php foreach ($governments as $govt) : ?>
+      <li>
+        <a href="admin/government" data="govtid=<?php echo $govt->id;?>" class="page"
+          style="background: <?php echo $govt->color1;?>; color: <?php echo $govt->color2;?>;">
+          <?php echo $govt->name;?>
+        </a>
+      </li>
+    <?php endforeach; ?>
   </ul>
 </div>
+<?php endif;?>
