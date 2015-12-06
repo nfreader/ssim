5a34f4df53b047f9bb71398103c0455159e330ba
Nick Farley <nick@nfreader.net>
2015-12-06 12:42:03 -0500

    Distress beacons are fixed
    Rejoice!
    
    Also replaced some table prefixes

--- inc/classes/beacon.php
+++ inc/classes/beacon.php
@@ -4,12 +4,12 @@ class beacon {
   public function getBeacons($syst) {
     $this->beaconCleanUp();
     $db = new database();
-    $db->query("SELECT ssim_beacon.*,
-    (ADDDATE(ssim_beacon.timestamp, INTERVAL 1 WEEK)) AS expires,
-    ssim_pilot.name
-    FROM ssim_beacon
-    LEFT JOIN ssim_pilot ON ssim_beacon.placedby = ssim_pilot.uid
-    WHERE ssim_beacon.syst = ?");
+    $db->query("SELECT tbl_beacon.*,
+    (ADDDATE(tbl_beacon.timestamp, INTERVAL 1 WEEK)) AS expires,
+    tbl_pilot.name
+    FROM tbl_beacon
+    LEFT JOIN tbl_pilot ON tbl_beacon.placedby = tbl_pilot.uid
+    WHERE tbl_beacon.syst = ?");
     $db->bind(1,$syst);
     $db->execute();
     return $db->resultSet();
@@ -18,8 +18,8 @@ class beacon {
   public function beaconCleanUp() {
     $db = new database();
     $db->query("DELETE FROM tbl_beacon
-      WHERE tbl_beacon.timestamp < ADDDATE(NOW(), INTERVAL 1 WEEK)
-      AND tbl_beacon.type = 'D'");
+      WHERE tbl_beacon.timestamp < SUBDATE(NOW(), INTERVAL 1 WEEK)
+      AND tbl_beacon.type = 'D';");
     try {
       $db->execute();
     } catch (Exception $e) {
@@ -33,22 +33,25 @@ class beacon {
       WHERE placedby = ? AND syst = ? AND type = 'D'");
     $db->bind(1,$pilot);
     $db->bind(2,$syst);
-    $db->execute();
-    return $db->single();
+    try {
+      $db->execute();
+    } catch (Exception $e) {
+      return returnError("Database error: ".$e->getMessage());
+    }
+    return $db->single()->beacons;
   }
 
   public function newDistressBeacon() {
     $this->beaconCleanUp();
-    $pilot = new pilot();
-    $beacons = $this->hasDistressBeacon($pilot->uid,
-      $pilot->syst);
-    if( $beacons->beacons > 0) {
+    $pilot = new pilot(NULL);
+    if($this->hasDistressBeacon($pilot->uid,
+      $pilot->syst)) {
       return returnError("Unable to launch more beacons.");
     } else {
-      $msg = "Pan-pan, pan-pan, pan-pan, this is <em>BSV ".$pilot->vessel->name."</em> transmitting on the blind guard. I am out of fuel in the $pilot->systname system and require immediate assistance. Any vessel receving, please respond.";
+      $msg = "Pan-pan, pan-pan, pan-pan, this is <em> ".$pilot->vessel->name."</em> transmitting on the blind guard. I am out of fuel in the $pilot->systname system and require immediate assistance. Any vessel receiving, please respond.";
       $db = new database();
-      $db->query("INSERT INTO ssim_beacon (placedby, syst, content, type,   timestamp)
-        VALUES (:placedby, :syst, :content, 'D', NOW())");
+      $db->query("INSERT INTO tbl_beacon (placedby, syst, content, `type`)
+        VALUES (:placedby, :syst, :content, 'D')");
       //Place one in the current system
       $db->bind(':placedby',$pilot->uid);
       $db->bind(':syst',$pilot->syst);
--- view/meta/test.php
+++ view/meta/test.php
@@ -2,5 +2,5 @@
 
 require_once('../../inc/config.php');
 $beacon = new beacon();
-var_dump($beacon->beaconCleanUp());
+var_dump($beacon->hasDistressBeacon('40abc4d3a95',0));
 ?>
